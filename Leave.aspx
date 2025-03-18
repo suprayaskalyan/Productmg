de55441be7d7fe1e6bad34cd1a28c9519118454d
Suprayas Kalyan <59998543+suprayaskalyan@users.noreply.github.com>
2025-03-19 00:38:46 +0530

    Add files via upload

---
+++ Leave.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Leave.aspx.cs" Inherits="LeaveManagement.Leave" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Leave Apply</title>
+        <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <script>
+        $(document).ready(function () {
+            alert("hello");
+            $("#<%=ddlEmployees.ClientID%>").change(function () {
+                var empid = $(this).val();
+                alert(empid);
+                $.ajax({
+                    type: "POST",
+                    url: "Leave.aspx/GetLeaveTypes",
+                    data: JSON.stringify({ id: empid }),
+                    contentType: "application/json",
+                    dataType: "json",
+                    success: function (response) {
+                        var options = '<option value="">Select Leave</option>';
+                        $.each(response.d, function (index, item) {
+                            options += '<option value="' + item.leave_id + '">' + item.leave_name + '</option>';
+                        });
+                        $("#<%= ddlLeaveType.ClientID %>").html(options);
+                    },
+                    error: function (xhr, status, error) {
+                        console.log("AJAX Error:" + error);
+                    }
+                });
+            });
+        });
+    </script>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Leave Apply</h2>
+            Employee Name:<asp:DropDownList ID="ddlEmployees" runat="server"></asp:DropDownList><br />
+            Leave Apply:<asp:DropDownList ID="ddlLeaveType" runat="server"></asp:DropDownList><br />
+            No.of Leave:<asp:TextBox ID="ddlNoOfLeave" runat="server"></asp:TextBox><br />
+            <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Leave.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LeaveManagement
+{
+    public partial class Leave : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                LoadEmployees();
+            }
+        }
+        private void LoadEmployees()
+        {
+            string connString = ConfigurationManager.ConnectionStrings["mycon"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                string query = "SELECT * FROM employee";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    conn.Open();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    
+                    ddlEmployees.DataSource = dt;
+                    ddlEmployees.DataTextField = "emp_name";
+                    ddlEmployees.DataValueField = "emp_id";
+                    ddlEmployees.DataBind();
+                }
+            }
+        }
+        [WebMethod]
+        public static List<LeaveType>GetLeaveTypes(string id)
+        {
+            List<LeaveType> leaveList = new List<LeaveType>();
+            string connString = ConfigurationManager.ConnectionStrings["mycon"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                string query = @"SELECT lm.leave_id, lm.leave_name FROM leave_master lm INNER JOIN leave_emp_assign lea ON lm.leave_id=lea.leave_id WHERE lea.emp_id =@empid";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@empid", id);
+                    conn.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        leaveList.Add(new LeaveType
+                        {
+                            leave_id = reader["leave_id"].ToString(),
+                            leave_name = reader["leave_name"].ToString()
+                        });
+                    }
+                }
+            }
+            return leaveList;
+        }
+        public class LeaveType
+        {
+            public string leave_id { get; set; }
+            public string leave_name { get; set; }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
